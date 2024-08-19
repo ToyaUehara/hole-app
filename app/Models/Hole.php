92db1356a7264e9ba90ae835e38938ef948bc178
ToyaUehara <ueha108@outlook.jp>
2024-08-19 20:31:20 +0900

    model's relation

--- app/Models/Hole.php
+++ app/Models/Hole.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Hole extends Model
 {
     use HasFactory;
+    
+    public function posts()
+    {
+        return $this->hasMany(Post::class);
+    }
 }
